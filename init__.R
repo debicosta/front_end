6480ab74b0d3612dfe4d9b5672f028c42c3c54e8
debicosta <60646008+debicosta@users.noreply.github.com>
2022-09-27 09:56:03 +0200

    Update init__.R

--- init__.R
+++ init__.R
@@ -8,14 +8,14 @@
 # Output: front end running logic
 ###################################################################################################
 
-# sourcing necessary files ----
-source("src/config.R")
-source("src/run_shiny.R")
 # install necessary packages --- it would be better to create a function
 library(shiny)
 library(httr)
 library(dplyr)
 library(jsonlite)
+# sourcing necessary files ----
+source("src/config.R")
+source("src/run_shiny.R")
 
 # running the front-end ---
 run_shiny_front(external_ip, port)
